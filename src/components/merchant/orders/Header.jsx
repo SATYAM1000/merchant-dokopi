b9f723314de3836f196c76d1abf57edb0d71c803
satyamx <satyamsingh748846@gmail.com>
2024-06-08 03:50:01 +0530

    show user ame on orders page

--- src/components/merchant/orders/Header.jsx
+++ src/components/merchant/orders/Header.jsx
@@ -1,10 +1,15 @@
+"use client";
+import { useCurrentUser } from "@/hooks/use-current-user";
 import { Search } from "lucide-react";
 import React from "react";
 
 const Header = () => {
+  const currentUser = useCurrentUser();
   return (
     <section className="w-full">
-      <h1 className="text-xl font-bold">Merchant Dashboard</h1>
+      <h1 className="text-xl font-bold capitalize">
+        Hello, {currentUser?.name.split(" ")[0]}
+      </h1>
       <p className="text-sm">Welcome to Merchant Dashboard</p>
       <div className="w-full overflow-hidden flex items-center rounded-sm bg-white border border-gray-800/[0.30] mt-4 gap-2 px-3 py-1">
         <Search className="w-4 h-4 text-gray-800" />
