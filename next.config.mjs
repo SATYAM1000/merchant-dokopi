b081187a6a306aa640b90d45a7464123d164bca4
satyamx <satyamsingh748846@gmail.com>
2024-06-29 22:01:35 +0530

    changes in settlements

--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,46 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "lh3.googleusercontent.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.unsplash.com",
-          port: "",
-          pathname: "/**",
-        },
-        {
-          protocol: "https",
-          hostname: "i.ytimg.com",
-          port: "",
-          pathname: "/**",
-        },
-        {
-          protocol: "https",
-          hostname: "example.com",
-          port: "",
-        },
-        {
-          protocol: "https",
-          hostname: "encrypted-tbn0.gstatic.com",
-          port: "",
-          pathname: "/**",
-        },{
-          protocol: "https",
-          hostname:"placehold.co",
-          port: "",
-          pathname: "/**",
-        }
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "user-images.githubusercontent.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
--- src/components/merchant/analytics/SettlementsCard.jsx
+++ src/components/merchant/analytics/SettlementsCard.jsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const SettlementsCard = () => {
   return (
     <div className="w-full lg:w-2/5 h-full p-6 border bg-white shadow-sm rounded-md flex flex-col gap-4">
       <p className=" font-medium text-black ">Last 10 Settlements</p>
-      <div className="w-full h-full flex items-center justify-center">
-        <p className="text-center font-medium text-gray-600">No settlements yet</p>
+      <div className="w-full h-full flex items-center justify-center flex-col gap-6 ">
+        <Image
+          src="https://user-images.githubusercontent.com/507615/54591670-ac0a0180-4a65-11e9-846c-e55ffce0fe7b.png"
+          alt="settlements"
+          width={200}
+          height={200}
+        />
+        <p className="text-center text-[#4D4D4D]">No settlements yet</p>
       </div>
     </div>
   );
