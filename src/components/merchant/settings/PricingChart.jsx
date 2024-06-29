d9037f1de2f541c27730f21f11ba2091573faac6
satyamx <satyamsingh748846@gmail.com>
2024-06-30 02:46:24 +0530

    pricing ui updated

--- src/components/merchant/settings/PricingChart.jsx
+++ src/components/merchant/settings/PricingChart.jsx
@@ -1,11 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchPricing, addPricingEntry } from "@/redux/store/pricingSlice";
-import axios from "axios";
-import AddConditionModal from "./AddConditionaModal";
-import { Button } from "@/components/ui/button";
-
+import React, { useState } from "react";
 import {
   Select,
   SelectContent,
@@ -14,86 +8,17 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+import AddConditionModal from "./AddConditionModal";
 
-const PricingChart = ({ storeId }) => {
-  const dispatch = useDispatch();
-  const { pricing } = useSelector((state) => state.pricing);
-
-  const [serviceType, setServiceType] = useState("");
-  const [paperSize, setPaperSize] = useState("");
-  const [basePrice, setBasePrice] = useState("");
-  const [conditions, setConditions] = useState([]);
+const PricingChart = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    dispatch(fetchPricing(storeId));
-  }, [dispatch, storeId]);
-
-  const handleAddCondition = (condition) => {
-    setConditions([...conditions, condition]);
-  };
-
-  const handleSavePricing = async () => {
-    const newPricingEntry = {
-      serviceType,
-      paperSize,
-      basePrice,
-      conditions,
-    };
-
-    try {
-      // await axios.post("/api/pricing", {
-      //   storeId,
-      //   ...newPricingEntry,
-      // });
-      dispatch(addPricingEntry(newPricingEntry));
-      resetForm();
-    } catch (error) {
-      console.error("Error saving pricing", error);
-    }
-  };
-
-  const resetForm = () => {
-    setServiceType("");
-    setPaperSize("");
-    setBasePrice("");
-    setConditions([]);
-  };
+  const [pricing, setPricing] = useState({});
 
   return (
-    <div className="">
-      <div className="mb-4 flex items-center gap-12 pb-6">
-        <div className="mb-4">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="service-type"
-          >
-            Service Type
-          </label>
-          <Select
-            onValueChange={(value) => setServiceType(value)}
-            defaultValue={serviceType}
-          >
-            <SelectTrigger className="w-[200px] font-medium text-[13px] appearance-none bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-              <SelectValue placeholder="Select service type" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="bw_copy">B/W Copy</SelectItem>
-                <SelectItem value="simple_color_copy">
-                  Simple Color Copy
-                </SelectItem>
-                <SelectItem value="digital_color_copy">
-                  Digital Color Copy
-                </SelectItem>
-                <SelectItem value="glossy_paper">Glossy Paper</SelectItem>
-                <SelectItem value="scan">Scan</SelectItem>
-                <SelectItem value="cartridge_paper">Cartridge Paper</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="mb-4">
+    <section className="w-full h-fit">
+      <div className="w-full flex items-center gap-8">
+        <div className="">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             htmlFor="paper-size"
@@ -101,10 +26,7 @@ const PricingChart = ({ storeId }) => {
             Paper Size
           </label>
 
-          <Select
-            onValueChange={(value) => setPaperSize(value)}
-            defaultValue={paperSize}
-          >
+          <Select>
             <SelectTrigger className="w-[200px] font-medium text-[13px] appearance-none bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500  ">
               <SelectValue placeholder="Select paper size" />
             </SelectTrigger>
@@ -119,40 +41,80 @@ const PricingChart = ({ storeId }) => {
             </SelectContent>
           </Select>
         </div>
-        <div className="mb-4">
+
+        <div className="">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="print-type"
+          >
+            Print type
+          </label>
+
+          <Select>
+            <SelectTrigger className="w-[200px] font-medium text-[13px] appearance-none bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500  ">
+              <SelectValue placeholder="Select print type" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="black_and_white">
+                  Black and white print
+                </SelectItem>
+                <SelectItem value="simple_color">Simple color print</SelectItem>
+                <SelectItem value="digital_color">
+                  Digital color print
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div className="">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="print-sides"
+          >
+            Print sides
+          </label>
+
+          <Select>
+            <SelectTrigger className="w-[200px] font-medium text-[13px] appearance-none bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500  ">
+              <SelectValue placeholder="Select print sides" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="single_sided">Single sided</SelectItem>
+                <SelectItem value="double_sided">Double sided</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div className="">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             htmlFor="base-price"
           >
             Base Price
           </label>
-          <div className="flex">
+
+          <div className="flex items-center gap-12">
             <input
               type="number"
-              value={basePrice}
+              id="base-price"
+              placeholder="0"
               min="0"
-              onChange={(e) => setBasePrice(e.target.value)}
               className="mt-1 appearance-none w-[200px] font-medium text-[13px] bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             />
-            <div className="mt-1">
-              <Button onClick={() => setIsModalOpen(true)} className="ml-12">
-                Add Condition
-              </Button>
-            </div>
+
+            <Button onClick={() => setIsModalOpen(true)}>Add Conditions</Button>
           </div>
         </div>
       </div>
       <AddConditionModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
-        onSave={handleAddCondition}
       />
-      <div className="flex justify-end">
-        <Button onClick={handleSavePricing} className="py-2 px-4 rounded">
-          Save Pricing
-        </Button>
-      </div>
-    </div>
+    </section>
   );
 };
 
