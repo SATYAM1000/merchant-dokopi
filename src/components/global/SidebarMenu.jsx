afa095762c820e694f2ebbdc444bb4c7f831ce51
satyamx <satyamsingh748846@gmail.com>
2024-06-29 21:15:45 +0530

    changes in sidebar

--- src/components/global/SidebarMenu.jsx
+++ src/components/global/SidebarMenu.jsx
@@ -102,10 +102,15 @@ const menuItems = [
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
-        className="lucide lucide-bell flex-shrink-0 w-5 h-5"
+        className="lucide lucide-shopping-basket flex-shrink-0 w-5 h-5 "
       >
-        <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
-        <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+        <path d="m15 11-1 9" />
+        <path d="m19 11-4-7" />
+        <path d="M2 11h20" />
+        <path d="m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8a2 2 0 0 0 2-1.6l1.7-7.4" />
+        <path d="M4.5 15.5h15" />
+        <path d="m5 11 4-7" />
+        <path d="m9 11 1 9" />
       </svg>
     ),
   },
